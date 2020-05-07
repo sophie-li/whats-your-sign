3a39176d09ac4448ee354b3b12685b9da37d6269
Sophie Li <sophie.sy.li@gmail.com>
2020-05-07 11:34:18 -0400

    Updates

---
+++ precache-manifest.799b37321dfb3efff4d413b31b090b62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6d134d63a0ce964d678fc51149f468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e766a6aa937e3c4689d5",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "85c198139c0dbf0dce62",
+    "url": "/static/css/main.904ed591.chunk.css"
+  },
+  {
+    "revision": "e766a6aa937e3c4689d5",
+    "url": "/static/js/2.e1f03236.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e1f03236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c198139c0dbf0dce62",
+    "url": "/static/js/main.28d68324.chunk.js"
+  },
+  {
+    "revision": "11f19e0bf8935d0a3895",
+    "url": "/static/js/runtime-main.374da2b0.js"
+  },
+  {
+    "revision": "afeaf6797b6286b72b613ababc563807",
+    "url": "/static/media/BebasNeue-Regular.afeaf679.woff"
+  }
+]);
