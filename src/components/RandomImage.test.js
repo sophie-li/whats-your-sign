5725c212fc0b0d1116dac4321ab4e253feb69fb0
Sophie Li <sophie.sy.li@gmail.com>
2020-05-08 16:21:26 -0400

    Update RandomImage tests

--- src/components/RandomImage.test.js
+++ src/components/RandomImage.test.js
@@ -2,13 +2,17 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import RandomImage from './RandomImage';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<RandomImage />);
+});
+
 it('expect to render RandomImage component', () => {
-  expect(shallow(<RandomImage />)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
 
 it('correctly changes imageID', () => {
-  const wrapper = shallow(<RandomImage />);
-
   wrapper.find('[id="magic-img"]').simulate('click');
   let imageID = wrapper.state().imageID;
   expect(imageID).toBeGreaterThanOrEqual(1);
@@ -18,4 +22,18 @@ it('correctly changes imageID', () => {
   imageID = wrapper.state().imageID;
   expect(imageID).toBeGreaterThanOrEqual(1);
   expect(imageID).toBeLessThan(51);
+});
+
+it("won't update component if the imageID remains the same", () => {
+  const shouldUpdate = wrapper
+    .instance()
+    .shouldComponentUpdate({}, { imageID: 1 });
+  expect(shouldUpdate).toBe(false);
+});
+
+it('will update component if the imageID is different than previous one', () => {
+  const shouldUpdate = wrapper
+    .instance()
+    .shouldComponentUpdate({}, { imageID: 2 });
+  expect(shouldUpdate).toBe(true);
 });
